11438bde97e19dec570bcda2fb323d23859b0b17
panos1962 <panos1962@localhost>
2009-08-06 09:38:39 +0300

    Update

--- tools/makelib.sh
+++ tools/makelib.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "${0}: internal error" >&2
-	exit 1
-fi
+makelib() {
+	if [ $# -ne 2 ]; then
+		echo "${0}: internal error" >&2
+		exit 1
+	fi
 
-if [ ! -f "${2}" ]; then
-	echo "${0}: {2} not found" >&2
-	return 1
-fi
+	if [ ! -f "${2}" ]; then
+		echo "${0}: {2} not found" >&2
+		return 1
+	fi
 
-echo "Linking \`${1}' with \`${2}' library..."
-ld --strip-all -o ${1} -shared spawk.o ${2} || {
-	echo "${0}: ${2}: link failed" >&2
-	exit 1
+	echo "Linking \`${1}'
+	with \`${2}' library..."
+	ld --strip-all -o ${1} -shared spawk.o ${2} || {
+		echo "${0}: ${2}: link failed" >&2
+		return 1
+	}
+
+	selinuxenabled 2>/dev/null && chcon -t textrel_shlib_t ${1}
+	return 0
 }
 
-selinuxenabled 2>/dev/null && chcon -t textrel_shlib_t ${1}
-exit 0
+if [ $# -eq 2 ]; then
+	makelib "${1}" "${2}"
+	exit $?
+fi
+
+if [ $# -ne 0 ]; then
+	echo "usage: ${0} [awk_library MySQL_library]" >&2
+	exit 1
+fi
+
+errs=2
+for i in "" "_r"
+do
+	mysql=$(find / -name libmysqlclient${i}.so \
+-print -quit 2>/dev/null)
+	if [ -z "${mysql}" ]; then
+		echo "${0}: libmysqlclient${i}.so: \
+MySQL library not found.
+${0}: libspawk${i}.so: awk extension library not created!" >&2
+	else
+		makelib /usr/lib/libspawk${i}.so ${mysql} && errs=0
+	fi
+done
+exit ${errs}
